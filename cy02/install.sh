48342141d01135412cba39ef35b4455bdb158b2f
Jean-Michel <59661788+Jeanmichel7@users.noreply.github.com>
2023-09-03 17:47:41 +0200

    update script

--- cy02/install.sh
+++ cy02/install.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
 apt update && apt upgrade -y
 apt install -y nginx tor
+
 cp nginx.conf /etc/nginx/sites-available/default
 cp torrc /etc/tor/
 cp sshd_config /etc/ssh/sshd_config
@@ -10,7 +11,15 @@ cp index.html /var/www/html/
 systemctl restart nginx
 systemctl enable tor
 systemctl restart tor
+while ! systemctl is-active -q tor; do
+    sleep 1
+done
+
 systemctl restart sshd
 
-echo "Adresse .onion est:"
-cat /var/lib/tor/hidden_service/hostname
+ONION_ADDR=$(cat /var/lib/tor/hidden_service/hostname)
+sed -i "s/server_name .*.onion;/server_name $ONION_ADDR;/g" /etc/nginx/sites-available/default
+
+systemctl reload nginx
+
+echo "Adresse .onion est: $ONION_ADDR"
