970e1e27349723e6056c3e2ed9164845487fc94c
Jean-Michel <59661788+Jeanmichel7@users.noreply.github.com>
2023-09-03 15:29:08 +0200

    script conf debian

---
+++ cy02/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update && sudo apt upgrade -y
+apt install -y nginx tor
+cp nginx.conf /etc/nginx/sites-available/default
+cp torrc /etc/tor/
+cp sshd_config /etc/ssh/sshd_config
+cp index.html /var/www/html/
+
+systemctl restart nginx
+systemctl enable tor
+systemctl restart tor
+
+systemctl restart sshd
+
+echo "Adresse .onion est:"
+cat /var/lib/tor/hidden_service/hostname
