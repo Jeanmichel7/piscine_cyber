1534976e06063be100542610cecfc2fc1eeb4dd7
Jean-Michel <59661788+Jeanmichel7@users.noreply.github.com>
2023-09-03 16:17:00 +0200

    fix: conf

--- cy02/install.sh
+++ cy02/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 apt install -y nginx tor
 cp nginx.conf /etc/nginx/sites-available/default
 cp torrc /etc/tor/
@@ -10,7 +10,6 @@ cp index.html /var/www/html/
 systemctl restart nginx
 systemctl enable tor
 systemctl restart tor
-
 systemctl restart sshd
 
 echo "Adresse .onion est:"
